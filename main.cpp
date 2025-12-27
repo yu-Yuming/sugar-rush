f34baf3b5f6ac2ee1614e5307b3a5069941a8cf5
yu-Yuming <78905337+yu-Yuming@users.noreply.github.com>
2025-12-27 18:06:42 +0900

    basic setup

--- main.cpp
+++ main.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
+#include <vector>
+
+static std::vector<int> memory(32768, 0);
+static int ptr = 0;
 
 int main() {
     return 0;
+}
+
+void memLeft() {
+    ptr += 1;
+}
+
+void memRight() {
+    ptr -= 1;
+}
+
+void memInc() {
+    memory[ptr] += 1;
+}
+
+void memDec() {
+    memory[ptr] -= 1;
 }
