a394b23cd08d652b22eec1c56148785bde999228
yu-Yuming <78905337+yu-Yuming@users.noreply.github.com>
2025-12-27 22:29:04 +0900

    implement basic code interpretation logic

--- main.cpp
+++ main.cpp
@@ -1,19 +1,44 @@
 #include <iostream>
+#include <iterator>
 #include <vector>
+#include <sstream>
+
+void memRight();
+void memLeft();
+void memInc();
+void memDec();
 
 static std::vector<int> memory(32768, 0);
 static int ptr = 0;
 
 int main() {
-    return 0;
-}
+    /** Reading Code */
+    std::string code;
+    while (std::getline(std::cin, code)) {
+        std::stringstream ss(code);
+        std::vector<std::string> words{std::istream_iterator<std::string> (ss), std::istream_iterator<std::string> ()};
+        for (const std::string & word : words) {
+            if (word == "쨔무") {
+                memRight();
+            } else if (word == "냠무") {
+                memLeft();
+            } else if (word == "민트초코") {
+                memInc();
+            } else if (word == "오오") {
+                memDec();
+            }
+        }
+    }
 
-void memLeft() {
-    ptr += 1;
+    return 0;
 }
 
 void memRight() {
     ptr -= 1;
+}
+
+void memLeft() {
+    ptr += 1;
 }
 
 void memInc() {
